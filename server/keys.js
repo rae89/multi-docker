812e1ea643bab0a2faa491f2b81ad27a1dd522d2
Robert Espinoza <sooperdooper@Roberts-MacBook-Pro.local>
2019-05-29 21:37:01 -0700

    add docker-compose

--- server/keys.js
+++ server/keys.js
@@ -2,8 +2,8 @@ module.exports = {
     redisHost: process.env.REDIS_HOST,
     redisPort: process.env.REDIS_PORT,
     pgUser: process.env.PGUSER,
-    pgHost: process.env.pgHost,
+    pgHost: process.env.PGHOST,
     pgDatabase: process.env.PGDATABASE,
     pgPassword: process.env.PGPASSWORD,
     pgPORT: process.env.PGPORT
-};
+}; 
